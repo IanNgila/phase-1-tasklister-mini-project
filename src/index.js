00878b69fa4d63b77d0000fe0b38d9fe490fdc9b
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-24 15:22:22 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-    document.getElementById("create-task-form").addEventListener("submit", function(event){
-    event.preventDefault();
-    event.target[0].value;
-    let li = document.createElement("li");
-    li.textContent = event.target[0].value;
-    document.getElementById("tasks").append(li)
-    });
-  })
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+  const newTaskUl = document.getElementById("tasks");
+  newTaskForm.addEventListener("submit", createNewTask);
+});
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+  appendNewTask(newTask);
+  event.target.reset();
+};
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
